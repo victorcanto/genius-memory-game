620baf740b993217b4dbc2966809023cd92b535e
Victor Canto <iamvictorcanto@gmail.com>
2021-10-05 00:06:02 -0300

    fix: gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.ts',
-  "devtool": 'inline-source-map',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.(js|ts)$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Genius Game',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
